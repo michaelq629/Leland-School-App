28fe3f847b7b9e72ad403323c8c5a59a16f6df34
SnoopyDog <michael.que@icloud.com>
2019-10-23 17:05:10 -0700

    Initial Commit

---
+++ Actual ID Card App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actual ID Card App
+//
+//  Created by Michael Que on 10/23/19.
+//  Copyright © 2019 Michael Que. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
